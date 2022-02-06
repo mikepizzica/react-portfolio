db30d36a19f0652b7883271a8274ac1d4b1b7f87
Mike Pizzica <mike.pizzica@gmail.com>
2022-02-06 18:23:00 -0500

    update header and introduce CSS rules

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,21 @@ import Nav from './nav';
 
 function header() {
     return (
-        <>
-        <h1>this is the header</h1>
-        <Nav/>
-        </>
+        <header>
+        <h1>Mike Pizzica</h1>
+        <img src={process.env.PUBLIC_URL + '/assets/images/MikePizzica1000x1000.jpg'} alt="mike pizzica" />
+        <nav>
+            <li>
+                <a href="#about">About</a>
+            </li>
+            <li>
+                <a href="#work">Work</a>
+            </li>
+            <li>
+                <a href="#contact">Contact</a>
+            </li>
+        </nav>
+    </header>
     );
 }
 
