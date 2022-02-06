e5424ea65d24699a035f841cd5607fa2628516d7
Mike Pizzica <mike.pizzica@gmail.com>
2022-02-06 18:41:45 -0500

    update resume link

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,7 +11,7 @@ function Contact() {
                 <a href="mailto:mike.pizzica@gmail.com">mike.pizzica@gmail.com</a>
                 <a href="https://github.com/mikepizzica" target="_blank">GitHub</a>
                 <a href="https://www.linkedin.com/in/mikepizzica/" target="_blank">LinkedIn</a>
-                <a href="assets\Mike Pizzica Resume.pdf" target="_blank">Résumé</a>
+                <a href="https://docs.google.com/document/d/1M-TMOKGjUwbVCfruUq9cRhJBFnJ2jlQV/edit?usp=sharing&ouid=117672538973667414696&rtpof=true&sd=true" target="_blank">Résumé</a>
             </section>
         </article>
     )
