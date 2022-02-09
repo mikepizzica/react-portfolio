352184a871ec413095826b7de8b4c5a5639d5374
Mike Pizzica <mike.pizzica@gmail.com>
2022-02-09 13:33:13 -0500

    update homepage and paths in header and app

--- src/components/header.js
+++ src/components/header.js
@@ -8,19 +8,19 @@ function header() {
         {/* <img src={process.env.PUBLIC_URL + '/assets/images/MikePizzica1000x1000.jpg'} alt="mike pizzica" /> */}
         <nav>
 
-            <Link to="/react-portfolio/" className='text-link'>
+            <Link to="/" className='text-link'>
                 <a>About</a>
             </Link>
             
-            <Link to ="/react-portfolio/portfolio" className='text-link'>
+            <Link to ="/portfolio" className='text-link'>
                 <a>Portfolio</a>
             </Link>
 
-            <Link to ="/react-portfolio/contact" className='text-link'>
+            <Link to ="/contact" className='text-link'>
                 <a>Contact</a>
             </Link>
 
-            <Link to ="/react-portfolio/resume" className='text-link'>
+            <Link to ="/resume" className='text-link'>
                 <a>Resume</a>
             </Link>
 
