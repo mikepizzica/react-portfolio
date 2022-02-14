700fdcdb35df7842484cb1cff7ff3ccdee4d6575
Mike Pizzica <mike.pizzica@gmail.com>
2022-02-14 14:04:45 -0500

    improve functionality for hovering and links on mobile

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,13 +4,21 @@ import react from "react";
 // portfolio might be difficult - subcomponents and props.
 
 function Portfolio() {
+    var clickBtn = false;
+    const parentImg = document.getElementsByClassName('project-images')
+    function shoot(e) {
+        if(parentImg.hover) {
+            alert();
+            e.preventDefault();
+        }
+      }
     return (
             <section id="portfolio">
                 <h1>Portfolio</h1>
                 <div class="project">
                     <img src={process.env.PUBLIC_URL + '/assets/images/artistify.png'} alt="artistify" class="project-images" />
                     <header class="project-info">
-                        <a rel="noreferrer" href="https://artistify-project.herokuapp.com/" target="_blank" class="project-names">Artistify</a><p>&nbsp;&nbsp;</p>
+                        <a rel="noreferrer" onClick={shoot} href="https://artistify-project.herokuapp.com/" target="_blank" class="project-names">Artistify</a><p>&nbsp;&nbsp;</p>
                         <a rel="noreferrer" href="https://github.com/mikepizzica/artistify" target="_blank">
                         <img src={process.env.PUBLIC_URL + '/assets/images/github-logo.png'} alt="github logo" class="github-logos" />
                         </a>
@@ -19,7 +27,7 @@ function Portfolio() {
                 <div class="project">
                     <img src={process.env.PUBLIC_URL + '/assets/images/password-generator.png'} alt="password generator" class="project-images" />
                     <header class="project-info">
-                        <a rel="noreferrer" href="https://mikepizzica.github.io/password-generator/" target="_blank" class="project-names">PW Generator</a><p>&nbsp;&nbsp;</p>
+                        <a rel="noreferrer" onClick={shoot} href="https://mikepizzica.github.io/password-generator/" target="_blank" class="project-names">PW Generator</a><p>&nbsp;&nbsp;</p>
                         <a rel="noreferrer" href="https://github.com/mikepizzica/password-generator" target="_blank">
                         <img src={process.env.PUBLIC_URL + '/assets/images/github-logo.png'} alt="github logo" class="github-logos" />
                         </a>
@@ -28,7 +36,7 @@ function Portfolio() {
                 <div class="project">
                     <img src={process.env.PUBLIC_URL + '/assets/images/weather-dashboard.png'} alt="weather dashboard" class="project-images"/>
                     <header class="project-info">
-                        <a rel="noreferrer" href="https://mikepizzica.github.io/weather-dashboard/" target="_blank" class="project-names">Weather Info</a><p>&nbsp;&nbsp;</p>
+                        <a rel="noreferrer" onClick={shoot} href="https://mikepizzica.github.io/weather-dashboard/" target="_blank" class="project-names">Weather Info</a><p>&nbsp;&nbsp;</p>
                         <a rel="noreferrer" href="https://github.com/mikepizzica/weather-dashboard" target="_blank">
                         <img src={process.env.PUBLIC_URL + '/assets/images/github-logo.png'} alt="github logo" class="github-logos" />
                         </a>
@@ -37,7 +45,7 @@ function Portfolio() {
                 <div class="project">
                     <img src={process.env.PUBLIC_URL + '/assets/images/note-taker.png'} alt="note taker" class="project-images"/>
                     <header class="project-info">
-                        <a rel="noreferrer" href="https://nameless-ravine-92026.herokuapp.com/" target="_blank" class="project-names">Note Taker</a><p>&nbsp;&nbsp;</p>
+                        <a rel="noreferrer" onClick={shoot} href="https://nameless-ravine-92026.herokuapp.com/" target="_blank" class="project-names">Note Taker</a><p>&nbsp;&nbsp;</p>
                         <a rel="noreferrer" href="https://github.com/mikepizzica/note-taker" target="_blank">
                         <img src={process.env.PUBLIC_URL + '/assets/images/github-logo.png'} alt="github logo" class="github-logos" />
                         </a>
