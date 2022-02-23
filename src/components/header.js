ff36576ee0c7e99a6fba8845ba1d6c0447e41174
Mike Pizzica <mike.pizzica@gmail.com>
2022-02-22 21:39:47 -0500

    remove unnecessary comment

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,6 @@ function header() {
     return (
         <header>
         <h1>Mike Pizzica</h1>
-        {/* <img src={process.env.PUBLIC_URL + '/assets/images/MikePizzica1000x1000.jpg'} alt="mike pizzica" /> */}
         <nav>
 
             <NavLink to="/" className='text-NavLink' activeClassName="current" exact>
