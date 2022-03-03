59c50faa85a859a8ee700ecf5f04e7a117b38a7c
Mike Pizzica <mike.pizzica@gmail.com>
2022-03-03 18:25:43 -0500

    update bio

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,7 +5,7 @@ function aboutMe() {
         <section id="about-me">
             <h1>About me</h1>
             <img src={process.env.PUBLIC_URL + '/assets/images/MikePizzica1000x1000.jpg'} alt="mike pizzica" id="headshot" />
-            <p>Full-Stack Web Developer with an extensive background in Analytics, Digital Marketing, and Customer Service. On pace to earn a Full-Stack Web Development Certificate from Penn LPS and have developed skills in HTML5, CSS3, JavaScript, jQuery, command line, API consumption, MySQL, MongoDB and more. In my six years working in Analytics for a Philly-based Digital Marketing company I managed millions of dollars in ad spend, grew teams, and improved processes. With my new skills I’m looking to take on new challenges, build new things, and make meaningful, positive impacts on future projects.</p>
+            <p>Full-Stack Web Developer with an extensive background in Analytics, Digital Marketing, and Customer Service. Recently earned a Full-Stack Web Development Certificate from Penn LPS and have developed skills in HTML, CSS, JavaScript, jQuery, Bootstrap, Node.js, MySQL, MongoDB, Express, Handlebars, and React. In my six years working in Analytics for a Philly-based Digital Marketing company I managed millions of dollars in ad spend, grew teams, and improved processes. With my new skills I’m looking to take on new challenges, build new things, and make meaningful, positive impacts on future projects.</p>
         </section>
     );
 }
